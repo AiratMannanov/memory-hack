f330fcfa9cdb57b2e6ee6b14db8c92e49d59d59e
Airat <parkourtes@gmail.com>
2020-04-04 17:06:45 +0300

    add forms

---
+++ src/components/AddInfoPage/FormSide.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+import './formSide.scss'
+
+const FormSide = () => {
+  return (
+    <div className="form-side">
+      <p>Добавление информации<br />о Герое войны</p>
+      <form>
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default FormSide
