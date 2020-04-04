e353702caf5badf4a49f608b9218407963b51db7
Daniil <Danyant@mail.ru>
2020-04-04 16:42:48 +0200

    nothing(

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from 'react';
+import './components/Photo/Photo.css'
+const PhotoList = React.lazy(() => import('./components/PhotoList/PhotoList'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={'loading'}>
+        <PhotoList />
+      </Suspense>
+    </>
   );
 }
 
---
+++ src/components/Photo/Photo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Photo(props) {
+  const {styleClass} = props
+
+  return (
+    <>
+      <div className={styleClass}>
+          <img />
+      </div>
+    </>
+  )
+}
+
+export default Photo
+
