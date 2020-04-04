b3edb2d17e5c104b557aba1fe7e4b0460b13cb21
Airat <parkourtes@gmail.com>
2020-04-04 20:47:26 +0300

    add state

--- src/components/AddInfoPage/FormSide.js
+++ src/components/AddInfoPage/FormSide.js
@@ -1,47 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import './formSide.scss'
 
 const FormSide = () => {
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const middleName = useRef(null);
+  const dateOfBirthday = useRef(null);
+  const dateOfDeath = useRef(null);
+  const dateOfСonscription = useRef(null);
+  const motherCity = useRef(null);
+  const dutyPlace = useRef(null);
+  const militaryRank = useRef(null);
+  const historyAboutPerson = useRef(null);
+  const additionalLinks = useRef(null);
+  const email = useRef(null);
+
+
   return (
     <div className="form-side">
       <p>Добавление информации<br />о Герое войны</p>
       <form>
         <div className="inputGroup">
-          <input type="text" placeholder="Фамилия" />
-          <input type="text" placeholder="Имя" />
+          <input type="text" ref={firstName} placeholder="Фамилия" required />
+          <input type="text" ref={lastName} placeholder="Имя" required />
         </div>
 
         <div className="inputGroup">
-          <input type="text" placeholder="Отчество" />
-          <input type="text" placeholder="Год / Дата рождения" />
+          <input type="text" ref={middleName} placeholder="Отчество" />
+          <input type="text" ref={dateOfBirthday} placeholder="Год / Дата рождения" />
         </div>
 
         <div className="inputGroup">
-          <input type="text" placeholder="Местро рождения / Область" />
-          <input type="text" placeholder="Местро призыва / Область" />
+          <input type="text" ref={motherCity} placeholder="Местро рождения / Область" />
+          <input type="text" ref={placeOfRecruitment} placeholder="Местро призыва / Область" />
         </div>
 
         <div className="inputGroup">
-          <input type="text" placeholder="Год / Дата призыва" />
-          <input type="text" placeholder="Местро службы" />
+          <input type="text" ref={dateOfСonscription} placeholder="Год / Дата призыва" />
+          <input type="text" ref={dutyPlace} placeholder="Местро службы" />
         </div>
 
         <div className="inputGroup">
-          <input type="text" placeholder="Воинское звание" />
-          <input type="text" placeholder="Год / Дата смерти" />
+          <input type="text" ref={militaryRank} placeholder="Воинское звание" />
+          <input type="text" ref={dateOfDeath} placeholder="Год / Дата смерти" />
         </div>
 
         <div className="inputGroup singleInput">
-          <input type="text" placeholder="История героя" />
+          <input type="text" ref={historyAboutPerson} placeholder="История героя" />
         </div>
 
         <div className="inputGroup singleInput">
-          <input type="text" placeholder="Ссылки со сторонних проектов" />
+          <input type="text" ref={additionalLinks} placeholder="Ссылки со сторонних проектов" />
         </div>
 
         <div className="inputGroup singleInput">
-          <input type="text" placeholder="E-mail для доступа в личный кабинет" />
+          <input type="email" ref={email} placeholder="E-mail для доступа в личный кабинет" required />
         </div>
         <button type="submit" >Сохранить</button>
       </form>
--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -1,17 +1,32 @@
+import { SET_INFO } from '../actions/action-types'
+
 const initialState = {
   personInfo: {
     firstName: null,
     lastName: null,
     middleName: null,
     dateOfBirthday: null,
-    dateOfDead: null,
+    dateOfDeath: null,
+    dateOfСonscription: null,
+    motherCity: null,
+    placeOfRecruitment: null,
+    dutyPlace: null,
+    militaryRank: null,
+    historyAboutPerson: null,
+    additionalLinks: null,
+    email: null,
+    images: null
   }
 }
 
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case SET_INFO:
+      return {
+        ...state,
+        personInfo: { ...state.personInfo, ...action.payload }
+      }
     default:
       return {
         ...state
